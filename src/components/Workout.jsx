ff09d3332ab140de5c32992e1778a0aa37953fba
Nikitahh <nikitahsieh03@gmail.com>
2025-04-24 20:03:58 +0800

    sets content w/ interaction

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -4,12 +4,12 @@ import SectionWrapper from './SectionWrapper'
 
 export default function Workout(props) {
 	const { workout } = props
+
 	return (
 		<SectionWrapper header={'Welcome to '} title={['The', 'Danger', 'Zone']}>
 			<div className="flex flex-col gap-4">
 				{workout.map((exercise, i) => {
-					return
-					;<ExerciseCard exercise={exercise} key={i} />
+					return <ExerciseCard i={i} exercise={exercise} key={i} />
 				})}
 			</div>
 		</SectionWrapper>
